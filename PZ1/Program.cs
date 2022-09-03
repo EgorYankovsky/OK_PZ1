a46eaaedd27cdbaef8df126dfeaf7cdb44c86ac2
Egor Yankovskij <eyankovkey@gmail.com>
2022-09-04 00:59:18 +0700

    Добавьте файлы проекта.

---
+++ PZ1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PZ1
+{
+	public class TSK1
+	{
+		public static void shuffleFY(string str)
+		{
+			char[] elems = str.ToCharArray();
+			Random rnd = new Random();
+			for(int i = 0; i < elems.Length; i++)
+			{
+				int j = rnd.Next(0, elems.Length - 1);
+				char elem = elems[j];
+				elems[j] = elems[i];
+				elems[i] = elem;
+			}
+			foreach (char elem in elems)
+				Console.Write(elem);
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string alph = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+			// 10 штук исключительно для демострации разноброса
+			for (int i = 0; i < 10; i++)
+			{
+				TSK1.shuffleFY(alph);
+				Console.WriteLine();
+			}
+		}
+	}
+}
